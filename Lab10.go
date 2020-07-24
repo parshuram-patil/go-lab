8690923b61746f2a7dbb59a2e5f3c4a8e7ddd22b
parshuram-patil <parshuram.patil@outlook.in>
2020-07-24 10:29:57 +0530

    Dynamic Slice

---
+++ Lab10.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := make([]int, 5)
+	fmt.Println("Slice = ", a, "Len = ", len(a), ", cap  = ", cap(a))
+
+	b := make([]string, 2, 5)
+	fmt.Println("Slice = ", b, "Len = ", len(b), ", cap  = ", cap(b))
+	b[0] = "aa"
+	b[1] = "bb"
+	b = append(b, "cc")
+	b = append(b, "dd")
+	b = append(b, "ee")
+	b = append(b, "6th")
+	b = append(b, "7th")
+	b = append(b, "8th")
+	b = append(b, "9th")
+	b = append(b, "10th")
+
+	fmt.Println("Slice = ", b, "Len = ", len(b), ", cap  = ", cap(b))
+	b = append(b, "11th")
+	fmt.Println("Slice = ", b, "Len = ", len(b), ", cap  = ", cap(b))
+}
