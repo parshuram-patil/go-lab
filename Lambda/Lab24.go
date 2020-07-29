66197d24e46c11727328adb233096410f273c605
parshuram-patil <parshuram.patil@outlook.in>
2020-07-29 13:04:19 +0530

    Lambda Funtion

---
+++ Lambda/Lab24.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(hello)
+}
+
+type MyEvent struct {
+	Name string `json:"uname"`
+	Age  int    `json:"uage"`
+}
+
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func hello(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
