4344436068051087f240147c7d522014ed3f92da
parshuram-patil <parshuram.patil@outlook.in>
2020-09-18 18:48:43 +0530

    Listing objects in S3 bucket

---
+++ Lab27.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	// files, err := ioutil.ReadDir(".")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, f := range files {
+	// 	fmt.Printf("%v - %v - %v \n", f.Name(), f.Size(), f.IsDir())
+	// }
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+
+	if err != nil {
+		fmt.Println("In err", err)
+		panic("Problem in getting connection to AWS")
+	} else {
+		svc := s3.New(sess)
+		input := &s3.ListObjectsInput{
+			Bucket: aws.String("psp-cross-acc-test"),
+			// MaxKeys: aws.Int64(2),
+		}
+
+		result, err := svc.ListObjects(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case s3.ErrCodeNoSuchBucket:
+					fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
+			return
+		}
+
+		fmt.Println(result)
+	}
+}
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
